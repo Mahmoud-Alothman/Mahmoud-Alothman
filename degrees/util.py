f57917a76fd6c0fece435882fb2d6cc8a126546f
Mahmoud-Alothman <116309777+Mahmoud-Alothman@users.noreply.github.com>
2022-12-04 19:52:17 +0300

    First commit for this cs50 project 0

--- degrees/util.py
+++ degrees/util.py
@@ -2,7 +2,7 @@ class Node():
     def __init__(self, state, parent, action):
         self.state = state
         self.parent = parent
-        self.action = action
+        self.action = action 
 
 
 class StackFrontier():
